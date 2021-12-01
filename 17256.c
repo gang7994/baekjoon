754ba26db457f762ef0a60e695c97779a6a35525
gang7994 <gang7994@naver.com>
2021-12-01 17:52:47 +0900

    # 17256 sol

---
+++ 17256.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a1,a2,a3;
+    int c1,c2,c3;
+    scanf("%d %d %d",&a1,&a2,&a3);
+    scanf("%d %d %d",&c1,&c2,&c3);
+    printf("%d %d %d",c1-a3,c2/a2,c3-a1);
+}
