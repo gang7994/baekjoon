eb4137ef3a15e912ffd7e766c7987cee7f8b9c5a
gang7994 <gang7994@naver.com>
2021-12-31 13:33:50 +0900

    Add sol

---
+++ 1309.py
@@ -0,0 +1,9 @@
+n = int(input())
+d = [0]*100001
+
+d[1] = 3
+d[2] = 7
+
+for i in range(3,n+1):
+    d[i] = (2*d[i-1] + d[i-2])%9901
+print(d[n])
---
+++ 1427.py
@@ -0,0 +1,5 @@
+arr = list(map(int, input()))
+arr.sort(reverse=True)
+
+for i in range(len(arr)):
+    print(arr[i], end='')
